e4363e756a992d8edd8dd48e48861d1e736723e0
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2024-11-17 23:37:10 +0530

    update: random-numbers.ts

--- src/utils/random-numbers.ts
+++ src/utils/random-numbers.ts
@@ -1,25 +1,27 @@
-export const randomNumbers = (length: number, max: number, min: number = 0) => {
+/**
+ *
+ * @param max upper bound, excluding this value
+ * @param min lower bound
+ * @returns
+ */
+export function getRandomNum(max: number, min = 0) {
+  return (Math.round(Math.random() * (max - min)) % (max - min)) + min;
+}
+
+export function randomNumbersArr(length: number, max: number, min: number = 0) {
   const nums = [];
 
   for (let i = min; i <= max; i++) nums.push(i);
 
-  nums.sort(() => (Math.random() > 0.5 ? 1 : -1));
-
-  return new Set(nums.slice(0, length));
-};
-
-export const randomNumbersArr = (length: number, max: number, min: number = 0) => {
-  const nums = [];
-
-  for (let i = min; i <= max; i++) nums.push(i);
-
-  nums.sort(() => (Math.random() > 0.5 ? 1 : -1));
+  nums.sort(() => 0.5 - Math.random());
 
   return nums.slice(0, length);
-};
+}
 
 export function randomElementFromArr(arr: any[]) {
-  const l = arr.length;
-
-  return arr[Math.round(Math.random() * (l - 1))];
+  return arr[Math.round(Math.random() * (arr.length - 1))];
 }
+
+export const randomNumbers = (length: number, max: number, min: number = 0) => {
+  return new Set(randomNumbersArr(length, max, min));
+};
