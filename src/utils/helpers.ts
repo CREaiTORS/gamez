8048bd3e68fa48164585d767c5792feb2a711746
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2025-04-11 02:32:06 +0530

    feat: add vibrate function and moveSVGPath utility for SVG manipulation

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -23,3 +23,37 @@ export function sleep(time: number) {
 export function diffTime(startTime: number, delay = 0) {
   return (new Date().getTime() - startTime - delay) / 1000;
 }
+
+export function vibrate(duration = 100) {
+  try {
+    navigator.vibrate(duration);
+  } catch {}
+}
+
+export function moveSVGPath(d: string, x: number, y: number) {
+  let regexp = /([a-zA-Z])([\s\d\-\.]*)/g;
+  return [...d.matchAll(regexp)]
+    .map((command) => {
+      let arr = command[2]
+        .trim()
+        .split(/\s/)
+        .map((val) => parseFloat(val));
+      let return_arr = arr;
+      switch (command[1]) {
+        case "M":
+        case "L":
+        case "H":
+        case "V":
+        case "Q":
+        case "T":
+          return_arr = [arr[0] + x, arr[1] + y];
+          break;
+        case "z":
+        case "Z":
+          return_arr = [];
+          break;
+      }
+      return `${command[1]}${return_arr.join(" ")}`;
+    })
+    .join(" ");
+}
