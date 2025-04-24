cf96d0ec6dc89c63af7fa47b85073d7c9fdc5778
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2025-04-25 00:10:50 +0530

    refactor: update CenterLoading component

--- src/components/CenterLoading.tsx
+++ src/components/CenterLoading.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { PiSpinnerBold } from "react-icons/pi";
 import { twMerge } from "tailwind-merge";
 
-interface props extends React.HTMLAttributes<HTMLDivElement> {
+export interface CenterLoadingProps extends React.HTMLAttributes<HTMLDivElement> {
   size?: number;
 }
 
-export const CenterLoading: React.FC<props> = ({ className, size = 72, ...props }) => (
-  <div className={twMerge("h-full w-full flex items-center justify-center", className)} {...props}>
-    <PiSpinnerBold size={size} className="animate-spin" />
-  </div>
-);
+export function CenterLoading({ className, size = 72, ...props }: CenterLoadingProps) {
+  return (
+    <div className={twMerge("h-full w-full flex items-center justify-center", className)} {...props}>
+      <PiSpinnerBold size={size} className="animate-spin" />
+    </div>
+  );
+}
