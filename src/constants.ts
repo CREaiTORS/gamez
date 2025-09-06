36fb56f731119e0c04e301cad4f17a6a8b6593c2
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2025-09-07 01:41:55 +0530

    refactor: remove LanguageType and related constants

--- src/constants.ts
+++
@@ -1,9 +0,0 @@
-import type { LanguageType } from "./types";
-
-export const LanguagesMap: Record<string, LanguageType> = {
-  en: "ENGLISH",
-  tl: "TAGALOG",
-  th: "THAI",
-  vi: "VIET",
-  in: "BAHASA",
-};
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,1 @@
-export type LanguageType = "ENGLISH" | "TAGALOG" | "THAI" | "VIET" | "BAHASA";
-
 export type ResultType = "" | "success" | "error" | "timeout";
