a4001b73af46cea8ae23163aac6f63889f475803
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2025-09-07 01:55:08 +0530

    refactor: remove CachedImage component and update exports in index.ts

--- src/components/CachedImage.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { getCachedImage } from "../lib/image-cache";
-
-export function CachedImage(props: React.ImgHTMLAttributes<HTMLImageElement> & { src: string }) {
-  const src = getCachedImage(props.src) ?? props.src;
-
-  return <img {...props} src={src} />;
-}
